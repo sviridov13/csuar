38f0c0db16b0e749ee957503dae3001521233d32
Nikita Sviridov <n.sviridov14@gmail.com>
2019-04-05 21:23:03 +0500

    Инициализация проекта

---
+++ app/src/main/java/cn/easyar/samples/helloar/SplashActivity.java
@@ -0,0 +1,14 @@
+package cn.easyar.samples.helloar;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
